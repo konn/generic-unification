cd2c2f888e5512afa15d4340a72aa5912145741e
Hiromi ISHII <konn.jinro_at_gmail.com>
2017-03-19 11:47:09 +0900

    Greatly simplifies internal implementation:
      * Get away with free monads
      * Changed internal representation of (uninstantiated) variables

--- src/Data/Unification/Generic.hs
+++ src/Data/Unification/Generic.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE DeriveTraversable, EmptyCase, FlexibleInstances, InstanceSigs #-}
-{-# LANGUAGE LambdaCase, PolyKinds, RankNTypes, ScopedTypeVariables        #-}
-{-# LANGUAGE StandaloneDeriving, TupleSections, TypeApplications           #-}
-{-# LANGUAGE UndecidableInstances, ViewPatterns                            #-}
+{-# LANGUAGE LambdaCase, PatternSynonyms, PolyKinds, RankNTypes            #-}
+{-# LANGUAGE ScopedTypeVariables, StandaloneDeriving, TupleSections        #-}
+{-# LANGUAGE TypeApplications, UndecidableInstances, ViewPatterns          #-}
 module Data.Unification.Generic
        ( -- $doc
 
@@ -9,12 +9,10 @@ module Data.Unification.Generic
          HasVar(..), Unifiable(..)
        ) where
 import           Control.Monad       (join)
-import           Control.Monad.Free
 import           Control.Monad.ST    (ST, runST)
 import           Control.Monad.State (StateT, gets, lift, modify, runStateT)
 import           Data.Map.Strict     (Map)
 import qualified Data.Map.Strict     as M
-import           Data.Maybe          (fromJust)
 import           Data.UnionFind.ST   (Point, descriptor, equivalent, fresh)
 import           Data.UnionFind.ST   (setDescriptor, union)
 import           GHC.Generics        ((:*:) (..), (:+:) (..), Generic1 (..))
@@ -27,14 +25,13 @@ data UnificationStatus f a =
 
 data UnificationError f a = OccursCheck (f a) (f a)
                           deriving (Read, Show, Eq, Ord)
-type Term = Free
 
 newtype Var f s a =
-  Var { runVar :: Point s (Maybe (Term f (Var f s a))) }
+  Var { runVar :: Point s (Either a (f (Var f s a))) }
   deriving (Eq)
 
 -- | Traversable functor with distinguishable /variable/ term.
---    
+--
 --   Default instance for @'HasVar' t@ considers every unary data constructor
 --   of form @Ctor v@ as variable term.
 class Traversable f => HasVar f where
@@ -73,34 +70,33 @@ instance GHasVar V1 where
 instance GHasVar (K1 i c) where
   gTakeVar _ = Nothing
 
-activateVars :: HasVar f => Free f a -> Free f a
-activateVars (Pure a) = Pure a
-activateVars (Free x) =
-  case takeVar x of
-    Nothing -> Free $ fmap (activateVars) x
-    Just a  -> activateVars a
+-- activateVars :: HasVar f => Free f a -> Free f a
+-- activateVars (Pure a) = Pure a
+-- activateVars (Free x) =
+--   case takeVar x of
+--     Nothing -> Free $ fmap (activateVars) x
+--     Just a  -> activateVars a
 
 abstractOrd :: (HasVar f, Ord a)
-            => Term f a
-            -> StateT (Map a (Var f s a)) (ST s) (Term f (Var f s a))
+            => f a
+            -> StateT (Map a (Var f s a)) (ST s) (f (Var f s a))
 abstractOrd = mapM $ \v -> do
   mvar <- gets (M.lookup v)
   case mvar of
     Nothing -> do
-      ref <- lift $ Var <$> fresh Nothing
+      ref <- lift $ Var <$> fresh (Left v)
       modify $ M.insert v ref
       return ref
     Just c  -> return c
 
-retrieve :: (Traversable f, Monad f, Ord a)
-         => Map a (Var f s a)
-         -> Var f s a
-         -> ST s (Term f a)
-retrieve dic = loop
+retrieve :: forall f s a. (HasVar f, Monad f)
+         => Var f s a
+         -> ST s (f a)
+retrieve = loop
   where
     loop (Var pt) = descriptor pt >>= \case
-      Nothing -> return $ Pure $ fromJust $ lookup (Var pt) [ (v, k) | (k, v) <- M.toList dic ]
-      Just t  -> join <$> mapM (retrieve dic) t
+      Left v  -> return $ V v
+      Right t -> join <$> mapM loop t
 
 -- | Unifiable functors.
 --
@@ -114,17 +110,16 @@ class Functor f => Unifiable f where
 
 unifyOrd :: (HasVar f, Monad f, Ord a, Generic1 f, GUnify f (Rep1 f))
          => f a -> f a -> Maybe (f a, Map a (f a))
-unifyOrd l0 r0 = runST $ do
-  let (l, r) = (activateVars $ liftF l0, activateVars $ liftF r0)
+unifyOrd l r = runST $ do
   ((l',r'), dic) <- runStateT ((,) <$> abstractOrd l <*> abstractOrd r) M.empty
   mans <- unify' l' r'
-  result <- mapM (fmap (retract . join) . mapM (retrieve dic)) mans
-  asig   <- mapM (fmap retract . retrieve dic) dic
+  result <- mapM (fmap (join) . mapM retrieve) mans
+  asig   <- mapM retrieve dic
   return ((, asig) <$> result)
 
 class (Functor f, Functor c) => GUnify f c where
-  gunify :: c (Free f (Var f s a)) -> c (Free f (Var f s a))
-         -> ST s (Maybe (c (Free f (Var f s a))))
+  gunify :: c (Var f s a) -> c (Var f s a)
+         -> ST s (Maybe (c (Var f s a)))
 
 instance GUnify f c => GUnify f (M1 i d c) where
   gunify (M1 l) (M1 r) = fmap M1 <$> gunify l r
@@ -142,7 +137,7 @@ instance (Functor f, Eq c) => GUnify f (K1 i c) where
     else return Nothing
 
 instance (Generic1 f, HasVar f, Monad f, GUnify f (Rep1 f)) => GUnify f (Rec1 f) where
-  gunify (Rec1 l) (Rec1 r) = fmap (Rec1 . return) <$> unify' (activateVars $ wrap l) (activateVars $ wrap r)
+  gunify (Rec1 l) (Rec1 r) = fmap Rec1 <$> unify' l r
 
 instance (GUnify f c, GUnify f d) => GUnify f (c :*: d) where
   gunify (l :*: r) (l' :*: r') = do
@@ -156,16 +151,17 @@ instance (GUnify f c, GUnify f d) => GUnify f (c :+: d) where
   gunify _ _ = return Nothing
 
 instance (Generic1 f, HasVar f, Monad f, GUnify f (Rep1 f)) => GUnify f Par1 where
-  gunify (Par1 l) (Par1 r) = fmap Par1 <$> unify' (activateVars l) (activateVars r)
+  gunify (Par1 l) (Par1 r) =
+    maybe Nothing (const $ Just $ Par1 l) <$> unify' (V l) (V r)
 
-occurs :: Traversable f => Var f s a -> f (Term f (Var f s a)) -> ST s Bool
-occurs (Var v) t = or <$> mapM (fmap or . mapM (equivalent v . runVar)) t
+occurs :: Traversable f => Var f s a -> f (Var f s a) -> ST s Bool
+occurs v t = or <$> mapM (equiv v) t
 
-readVar :: Var f s a -> ST s (Maybe (Term f (Var f s a)))
+readVar :: Var f s a -> ST s (Either a (f (Var f s a)))
 readVar = descriptor . runVar
 
-assign :: Var t1 s t -> Term t1 (Var t1 s t) -> ST s (Maybe (Term t1 (Var t1 s t)))
-assign (Var v) t = setDescriptor v (Just t) >> return (Just t)
+assign :: Var f s t -> f (Var f s t) -> ST s (Maybe (f (Var f s t)))
+assign (Var v) t = setDescriptor v (Right t) >> return (Just t)
 
 equate :: Var t s a -> Var t s a -> ST s ()
 equate (Var v) (Var u) = union v u
@@ -173,37 +169,46 @@ equate (Var v) (Var u) = union v u
 equiv :: Var t s a -> Var t s a -> ST s Bool
 equiv (Var v) (Var u) = equivalent v u
 
+tryTakeVar :: HasVar f => f v -> Either (f v) v
+tryTakeVar v = maybe (Left v) Right $ takeVar v
+
+pattern V :: (Applicative f, HasVar f) => Applicative f => v -> f v
+pattern V v <- (tryTakeVar -> Right v) where
+  V v = pure v
+
+pattern E :: HasVar f => f v -> f v
+pattern E t <- (tryTakeVar -> Left t) where
+  E t = t
+
 unify' :: forall f a s. (Generic1 f, HasVar f, Monad f, GUnify f (Rep1 f))
-       => Free f (Var f s a)
-       -> Free f (Var f s a)
-       -> ST s (Maybe (Term f (Var f s a)))
-unify' (Free t) (Free s) = do
+       => f (Var f s a)
+       -> f (Var f s a)
+       -> ST s (Maybe (f (Var f s a)))
+unify' (E t) (E s) = do
   let tRep = from1 t
       sRep = from1 s
-  fmap (wrap . to1) <$> gunify tRep sRep
-unify' (Free t) (Pure v) = do
+  fmap to1 <$> gunify tRep sRep
+unify' (E t) (V v) = do
   occ <- occurs v t
   readVar v >>= \case
-    Nothing | not occ -> assign v (Free t)
+    Left {} | not occ   -> assign v t
             | otherwise -> return Nothing
-    Just u -> unify' (Free t) u >>= \case
-      Nothing  -> return Nothing
-      (Just u') -> assign v u'
-unify' (Pure v) (Free s)  = unify' (Free s) (Pure v)
-unify' (Pure u) (Pure v) = do
+    Right u -> maybe (return Nothing) (assign v) =<< unify' t u
+unify' (V v) (E s)  = unify' s (V v)
+unify' (V u) (V v) = do
   eq <- equiv u v
   if eq
-    then return (Just (Pure u))
+    then return (Just (V u))
     else do
       u' <- readVar u
       v' <- readVar v
-      equate u v
       match u' v'
   where
-    match Nothing Nothing   = return (Just $ Pure u)
-    match Nothing (Just t)  = unify' (Pure u) t
-    match (Just s) Nothing  = unify' (Pure v) s
-    match (Just t) (Just s) = unify' t s
+    match Left{} Left{}     = equate u v >> return (Just $ V v)
+    match Left{} (Right t)  = unify' (V u) t
+    match (Right s) Left{}  = unify' (V v) s
+    match (Right t) (Right s) = mapM (return <* pure (equate u v)) =<< unify' t s
+unify' _ _ = error "Cannot happen!" -- GHC cannot infer this empty case
 
 {-$doc
 
@@ -244,7 +249,7 @@ __instance__ 'Monad' Expr __where__
   Var x    '>>=' f = f x
   Lit i    '>>=' _ = Lit i
   (l :+ r) '>>=' f = (l '>>=' f) :+ (l '>>=' r)
- 
+
 __instance__ 'Applicative' Expr __where__
   'pure'  = return
   ('<*>') = 'Control.Monad.ap'
